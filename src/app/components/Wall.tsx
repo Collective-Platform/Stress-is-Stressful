129b056247234da9816f15119c8838a9f9461722
Strictly Student <youth@collective.my>
2024-11-15 23:03:25 +0800

    Update Wall.tsx

--- src/app/components/Wall.tsx
+++ src/app/components/Wall.tsx
@@ -26,7 +26,7 @@ export default function Home() {
         </div>
         <div className="absolute inset-0 flex items-center justify-center p-4">
           <button
-            className="w-full max-w-md rounded-2xl border-b-4 border-r-4 border-oren-600 bg-gradient-to-b from-oren-2 to-oren-3 py-4 text-lg font-bold uppercase text-dark-blue shadow-[4px_4px_0_rgba(0,0,0,0.3)] transition-all duration-100 hover:scale-105 hover:shadow-lg active:translate-x-[4px] active:translate-y-[4px] active:border-b-0 active:border-r-0 active:shadow-none xl:text-xl"
+            className="w-full max-w-md rounded-2xl border-b-4 border-r-4 border-oren-600 bg-gradient-to-b from-oren-2 to-oren-3 py-4 text-sm font-bold uppercase text-dark-blue shadow-[4px_4px_0_rgba(0,0,0,0.3)] transition-all duration-100 hover:scale-105 hover:shadow-lg active:translate-x-[4px] active:translate-y-[4px] active:border-b-0 active:border-r-0 active:shadow-none xl:text-xl"
             onClick={navigateToStresswall}
           >
             Go to stresswall
