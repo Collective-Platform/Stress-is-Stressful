7bbcc78b2f442f0e952fa2501bbe802cd15b9403
Strictly Student <youth@collective.my>
2025-06-06 06:26:27 +0800

    Added google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,32 +26,26 @@ export default function RootLayout({
   return (
     <html className="h-full" lang="en">
       <head>
-        {/* Google Tag (gtag.js) */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-RPPB460CM1"
-          strategy="afterInteractive"
-        />
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: ` window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-RPPB460CM1'); `,
-          }}
-          id="google-analytics"
-          strategy="afterInteractive"
-        />
-
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `(function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "qfb544w9ps");`,
-          }}
-          id="clarity-script"
-          strategy="afterInteractive"
-        />
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MW9M4Z3F');
+          `}
+        </Script>
       </head>
       <body className={cn(pressstart2p.className, 'flex min-h-full flex-col')}>
+        <noscript>
+          <iframe
+            height="0"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MW9M4Z3F"
+            style={{ display: 'none', visibility: 'hidden' }}
+            width="0"
+          />
+        </noscript>
         {children}
         <Analytics />
         <SpeedInsights />
